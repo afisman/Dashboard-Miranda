37c4f8217a2790f715d50ed882f5b599566dbf0a
afisman <afisman@gmail.com>
2024-06-04 11:20:38 +0200

    test for deploy

--- src/components/layout/StyledLayout.tsx
+++ src/components/layout/StyledLayout.tsx
@@ -2,13 +2,10 @@ import styled from "styled-components";
 
 export const StyledLayout = styled.div<{ $sidebaropen: string }> `
     ${(props) =>
-        props.$sidebaropen === 'true' &&
-        `
-            margin-left: 17%;
-        `
+        props.$sidebaropen === 'true' && `margin-left: 17%;`
     }
 `;
 
 export const StyledContainer = styled.div`
-  display: flex;
+    display: flex;
 `;
